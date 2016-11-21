32b6ab8c333ddee4ac917f10786e40ddfc263d3c
HandsomeMachine <handsome.machine2014@gmail.com>
2016-11-21 09:00:14 +0300

    Правки 21.11.2016

---
+++ js/script.js
@@ -0,0 +1,37 @@
+var hotelSearch = document.querySelector(".search-form-btn");
+var popup = document.querySelector(".modal-content");
+
+var formSubmit = document.querySelector("form");
+var arrival = formSubmit.querySelector("[name=arrival-date]");
+var departure = formSubmit.querySelector("[name=departure-date]");
+var adults = formSubmit.querySelector("[name=adults-number]");
+var children = formSubmit.querySelector("[name=children-number]");
+
+var storage = localStorage.getItem("adults");
+
+hotelSearch.addEventListener("click", function(event) {
+	event.preventDefault();
+	popup.classList.toggle("modal-content-close");
+});
+
+formSubmit.addEventListener("submit", function(event) {
+	if (!arrival.value || !departure.value || !adults.value || !children.value) {
+	event.preventDefault();
+	popup.classList.remove("modal-content-error");
+	popup.offsetWidth = popup.offsetWidth;
+	popup.classList.add("modal-content-error");
+	} else {
+		localStorage.setItem("adults", adults.value);
+		localStorage.setItem("children", children.value);
+		popup.classList.remove("modal-content-error");
+	}
+});
+
+window.addEventListener("keydown", function(event) {
+	if (event.keyCode === 27) {
+		if (popup.classList.contains(!"modal-content-close")) {
+			popup.classList.add("modal-content-close");
+			popup.classList.remove("modal-content-error");
+		}
+	}
+});
